511aef8fd71684befe870a7d854bb8aa25692886
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-14 15:54:17 -0700

    Testing conditionals in js

---
+++ JavaScript/Conditionals/app.js
@@ -0,0 +1,10 @@
+
+let random = Math.random();
+if(random >= 0.5){
+    console.log("Your number is greater than 0.5");
+}
+if(random < 0.5){
+    console.log("Your number is less than 0.5");
+}
+console.log(random);
+
