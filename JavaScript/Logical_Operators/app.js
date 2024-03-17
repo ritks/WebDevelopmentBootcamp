1cea1ec01f7451aabe53dfa26d2b7c7d280d2230
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-17 15:59:47 -0700

    Practice with logical operators

---
+++ JavaScript/Logical_Operators/app.js
@@ -0,0 +1,21 @@
+const password = prompt("Enter you password");
+if(password.length <= 6 && password.indexOf(' ') === -1){
+    console.log("Valid Password");
+
+} else{
+    console.log("Invalid Password");
+}
+
+const age = 90;
+if( (age >= 0 && age <=5) || age >=65){
+    console.log("You get in for free");
+}
+else if (age > 5 && age < 10){
+    console.log("You get in for $10");
+}
+else if (age >= 10 && age < 65){
+    console.log("You get in for $20");
+}
+else{
+    console.log("Invalid age");
+}
