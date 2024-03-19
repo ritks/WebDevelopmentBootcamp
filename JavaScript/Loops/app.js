6bd86d04620061b5484b4733924d6417be1121d7
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-19 16:13:50 -0700

    Practice with nested loops

--- JavaScript/Loops/app.js
+++ JavaScript/Loops/app.js
@@ -1,3 +1,12 @@
 for(let i = 0; i <10; i++){
     console.log(i);
 }
+
+const seatingChart = [["James", "Alice", "Cooper"], ["Ronnie", "Max", "Tommy"], ["Holly", "Emily", "Brad"]];
+
+for(let i = 0; i < seatingChart.length; i++){
+    const row = seatingChart[i];
+    for(let j = 0; j < row.length; j++){
+        console.log(row[j]);
+    }
+}
