dc4f083a2077b3ccfca90e2746bf67af07186f7d
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-20 18:16:26 -0700

    Passing functions to higher order functions

--- JavaScript/Function_Expressions/app.js
+++ JavaScript/Function_Expressions/app.js
@@ -5,3 +5,18 @@ function add(x, y){
 const sum = function(x,y){
     return x+y;
 }
+
+
+// Higher order functions
+
+function callTwice(func){
+    func();
+    func();
+}
+
+function rollDie(){
+    const roll = Math.floor(Math.random() * 6) + 1;
+    console.log(roll);
+}
+
+callTwice(rollDie);
