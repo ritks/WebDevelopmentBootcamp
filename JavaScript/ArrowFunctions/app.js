f55db0370435698c81ec040a34628a6e8d58d623
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-21 18:16:00 -0700

    Practice with implicit returns in arrow functions

--- JavaScript/ArrowFunctions/app.js
+++ JavaScript/ArrowFunctions/app.js
@@ -3,14 +3,21 @@
 // }
 
 
-const add = (x,y) =>{
-    return x+y;
+const add = (x, y) => {
+    return x + y;
 }
 
-const square = (x) =>{
-    return x*x;
+const square = (x) => {
+    return x * x;
 }
 
-const rollDie = () =>{
-    return Math.floor(Math.random() * 6) + 1;
-}
+// const rollDie = () =>{
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+
+const rollDie = () => (
+    Math.floor(Math.random() * 6) + 1
+)
+
+const betterAdd = (x, y) => x + y;
+
