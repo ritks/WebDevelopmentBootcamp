7de26273c7f4a56ec3ddd2f536fad3d2bbc5fe6e
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-21 18:04:27 -0700

    Practice with map function (array callback)

--- JavaScript/ArrayCallbacks/app.js
+++ JavaScript/ArrayCallbacks/app.js
@@ -3,3 +3,9 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 numbers.forEach(function(el){
     console.log(el)
 });
+
+const doubles = numbers.map(function(num){
+    return num*2;
+});
+
+console.log(doubles);
