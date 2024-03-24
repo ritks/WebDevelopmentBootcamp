fc6e9bc8aa5d6860823c920aedfdfb95b075f7d2
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-24 16:25:30 -0700

    Practice with reduce callback function

--- JavaScript/ArrowFunctions/app.js
+++ JavaScript/ArrowFunctions/app.js
@@ -36,3 +36,15 @@ let id = setInterval(() => {
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13];
 const odds = numbers.filter(num => num % 2 === 1);
 // can combine methods like map/filter/some/every 
+
+
+//reduce puts the return value of the function as the second parameter of the next iteration
+const prices = [9.99, 1.50, 19.99, 49.99, 30.50];
+
+const total = prices.reduce((total, price) =>{
+    return total + price;
+})
+
+const min = prices.reduce((min, price) =>{
+    return Math.min(min, price);
+})
