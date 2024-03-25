17eea9bbd81b1665174d3c3c545237cd005bd656
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-24 17:06:56 -0700

    Practice with syntax for default parameters

---
+++ JavaScript/DefaultParams/app.js
@@ -0,0 +1,7 @@
+// function rollDie(numSides){
+//     return Math.floor(Math.random() * numSides) + 1;
+// }
+
+function rollDie(numSides = 6){
+    return Math.floor(Math.random() * numSides) + 1;
+}
