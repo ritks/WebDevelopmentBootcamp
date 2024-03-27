f5fcd83e95d6b1ae2ec15c6280407375bdf5ef3f
Ritik Singh <ritiksingh123456789@gmail.com>
2024-03-26 19:29:29 -0700

    Practice with element selection in the document

--- FullStackPractice/DOM/Selecting/app.js
+++ FullStackPractice/DOM/Selecting/app.js
@@ -1,10 +1,12 @@
 const allImages = document.getElementsByTagName('img');
 
-for (let img of allImages) {
-    img.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e2/Silky_bantam.jpg/440px-Silky_bantam.jpg'
+for(let img of allImages){
+    console.log(img.src);
 }
 
 
+// if the class or tag does not exist, an empty HTML collection is returned
+// as opposed to returning null like in getElementById
 const squareImages = document.getElementsByClassName('square');
 
 for (let img of squareImages) {
