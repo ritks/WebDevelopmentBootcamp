bd6ebb59716e1ec5ae9403e21165840c54e156d5
ritks <ritiksingh123456789@gmail.com>
2025-08-22 18:56:06 -0700

    Practice with creating elements and inserting elements

--- FullStackPractice/DOM/Manipulating/app.js
+++ FullStackPractice/DOM/Manipulating/app.js
@@ -23,11 +23,26 @@ h2.classList.toggle("purple");
 
 
 const firstBold = document.querySelector("b");
-console.log(firstBold.parentElement)
+console.log(firstBold.parentElement);
 const paragraph = firstBold.parentElement;
-console.log(paragraph.children)
+console.log(paragraph.children);
 
 let squareImg = document.querySelector(".square");
 // use nextElement sibling and prevElementSibling because nextSibling and prevSibling give nodes, which often contain white space and text nodes
-console.log(squareImg.nextElementSibling)
-console.log(squareImg.prevElementSibling)
+console.log(squareImg.nextElementSibling);
+console.log(squareImg.prevElementSibling);
+
+const newImage = document.createElement("img");
+newImage.src = "https://images.unsplash.com/photo-1563281577-a7be47e20db9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80";
+newImage.classList.add("square");
+
+const newH3 = document.createElement("h3");
+newH3.innerText = "I am NEW";
+document.body.appendChild(newH3);
+
+const p = document.querySelector("p");
+p.append("appending new text", "appending more text");
+const newB = document.createElement("b");
+newB.append("bolded text lol");
+document.querySelector("h1").insertAdjacentElement("afterend", newB);
+console.log(document.querySelector("h1").nextElementSibling === newB);
