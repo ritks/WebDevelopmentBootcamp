3fc8b84e7fc53687c700878acc4d7e752ece605e
ritks <ritiksingh123456789@gmail.com>
2025-08-22 20:08:29 -0700

    Practice with eventListener

--- FullStackPractice/DOM/Events/app.js
+++ FullStackPractice/DOM/Events/app.js
@@ -7,3 +7,18 @@ btn.onclick = function () {
 btn.onmouseenter = function () {
     console.log("AHHHHHHHH ENTERED");
 }
+
+const btn3 = document.querySelector("#V3");
+btn3.addEventListener("click", () => alert("clicked"))
+
+function twist () {
+    console.log("Twist");
+}
+function shout () {
+    console.log("shout");
+}
+
+const tasButton  = document.querySelector("#tas");
+// tasButton.onclick = twist;
+// tasButton.onclick = shout;
+tasButton.addEventListener("click", twist, shout)
