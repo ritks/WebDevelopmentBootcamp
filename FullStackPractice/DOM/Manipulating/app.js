9c462fe38023c7e0d04753f4f237a61c3cc3caf6
ritks <ritiksingh123456789@gmail.com>
2025-08-22 18:41:33 -0700

    Practice with js classList

--- FullStackPractice/DOM/Manipulating/app.js
+++ FullStackPractice/DOM/Manipulating/app.js
@@ -10,3 +10,14 @@ for (let link of allLinks) {
     link.style.textDecorationColor = 'magenta';
     link.style.textDecorationStyle = 'wavy'
 }
+
+const h2 = document.querySelector("h2");
+h2.setAttribute("class", "purple");
+h2.setAttribute("class", "border");
+
+h2.classList.add("purple");
+h2.classList.remove("border");
+h2.classList.contains("border");
+h2.classList.toggle("purple");
+h2.classList.toggle("purple");
+
