87a816447770f83f14b7095e22314cca79008fd9
ritks <ritiksingh123456789@gmail.com>
2025-08-22 20:02:32 -0700

    Practice with onclick and onmouseenter

---
+++ FullStackPractice/DOM/Events/app.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector("#V2");
+
+btn.onclick = function () {
+    console.log("You clicked v2");
+}
+
+btn.onmouseenter = function () {
+    console.log("AHHHHHHHH ENTERED");
+}
