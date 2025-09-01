906286b25d4304559f9a1f4ce295e82cd79f4ffa
ritks <ritiksingh123456789@gmail.com>
2025-09-01 01:55:19 -0700

    Practice with setting headers and combining axios with dom manipulation

--- FullStackPractice/Async/AJAX/Axios/app.js
+++ FullStackPractice/Async/AJAX/Axios/app.js
@@ -15,5 +15,24 @@ const getStarWarsPerson = async (id) => {
         console.log("ERROR", e)
     }
 }
-getStarWarsPerson(2);
-getStarWarsPerson(10);
+// getStarWarsPerson(2);
+// getStarWarsPerson(10);
+const jokes = document.querySelector("#jokes");
+const button = document.querySelector("button");
+const addNewJoke = async () => {
+    const jokeText = await getDadJoke();
+    const newLI = document.createElement("LI");
+    newLI.append(jokeText);
+    jokes.append(newLI)
+}
+button.addEventListener("click", addNewJoke)
+const getDadJoke = async () => {
+    try {
+        const config = { headers: { Accept: "application/json" } };
+        const res = await axios.get("https://icanhazdadjoke.com/", config);
+        // console.log(res.data.joke);
+        return res.data.joke
+    } catch (e) {
+        return "NO JOKES AVAILABLE, SORRY"
+    }
+}
