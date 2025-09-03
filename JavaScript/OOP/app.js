ab963faed1ae53e8981f392a32a40ded6ea10619
ritks <ritiksingh123456789@gmail.com>
2025-09-02 18:50:12 -0700

    Practice with constructor functions

--- JavaScript/OOP/app.js
+++ JavaScript/OOP/app.js
@@ -1,24 +1,46 @@
-function hex(r, g, b) {
+// function hex(r, g, b) {
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// }
+
+// function rgb (r, g, b) {
+//     return `rgb(${r}, ${g}, ${b})`
+// }
+// hex(255, 100, 25)
+
+// function makeColor(r, g, b){
+//     const color = {};
+//     color.r = r;
+//     color.g = g;
+//     color.b = b;
+//     color.rgb = function() {
+//         const {r,g,b} = this;
+//         return `rgb(${r}, ${g}, ${b})`
+//     };
+//     color.hex = function () {
+//         const {r,g,b} = this;
+//         return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//     }
+//     return color
+// }
+
+
+function Color(r, g, b) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.rgb = function() {
+        const {r, g, b} = this;
+        return `rgb(${r}, ${g}, ${b})`;
+    };
+}
+
+Color.prototype.hex = function (r, g, b) {
+    const {r, g, b} = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
 }
 
-function rgb (r, g, b) {
-    return `rgb(${r}, ${g}, ${b})`
+Color.prototype.rgba = function (a=1.0) {
+    const {r, g, b} = this;
+    return `rgb(${r}, ${g}, ${b}, ${a})`
 }
-hex(255, 100, 25)
-
-function makeColor(r, g, b){
-    const color = {};
-    color.r = r;
-    color.g = g;
-    color.b = b;
-    color.rgb = function() {
-        const {r,g,b} = this;
-        return `rgb(${r}, ${g}, ${b})`
-    };
-    color.hex = function () {
-        const {r,g,b} = this;
-        return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-    return color
-}
+new Color(255, 150, 5)
