ac8788df3963253bf3fef3638998d1e06d2adb5c
ritks <ritiksingh123456789@gmail.com>
2025-09-10 19:02:30 -0700

    Finished Color class

--- JavaScript/OOP/app.js
+++ JavaScript/OOP/app.js
@@ -51,6 +51,7 @@ class Color {
         this.g = g;
         this.b = b;
         this.name = name;
+        this.calcHSL();
     }
     innerRGB() {
         const { r, g, b } = this;
@@ -67,16 +68,64 @@ class Color {
         const { r, g, b } = this;
         return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
     }
-}
-const c1 = new Color(255, 67, 89, "tomato");
-const red = new Color(255, 0, 0, "red")
-const white = new Color(255, 255, 255, "white")
+    hsl() {
+        const{h, s, l} = this;
+        return `hsl(${h}, ${s}%, ${l}%)`
+    }
+    opposite() {
+        const{h, s, l} = this;
+        const newHue = (h + 180) % 360
+        return `hsl(${newHue}, ${s}%, ${l}%)`
+    }
+    fullySaturated () {
+        const{h, l} = this;
+        return `hsl(${h}, 100%, ${l}%)`
+    }
+    calcHSL() {
+        let {r, g, b} = this;
+        r /= 255;
+        g /= 255;
+        b /= 255;
 
-class Node {
-    constructor(key=-1, value=-1, next=null, prev=null) {
-        this.key = key;
-        this.value = value;
-        this.next = next;
-        this.prev = prev;
+        let cmin = Math.min(r, g, b),
+            cmax = Math.max(r, g, b),
+            delta = cmax - cmin,
+            h = 0,
+            s = 0,
+            l = 0;
+        
+        if (delta == 0) h = 0;
+        else if (cmax == r)
+            h = ((g - b) / delta) % 6;
+        else if (cmax == g)
+            h = (b - r) / delta + 2;
+        else
+            h = (r - g) / delta + 4;
+
+        h = Math.round(h * 60);
+
+        if (h < 0) h += 360;
+
+        l = (cmax + cmin) / 2;
+
+        s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+
+        s = +(s * 100).toFixed(1);
+        l = +(l * 100).toFixed(1);
+        this.h = h;
+        this.s = s;
+        this.l = l;
     }
 }
+const red = new Color(255, 0, 0, "tomato")
+const white = new Color(255, 255, 255, "white")
+
+// class Node {
+//     constructor(key=-1, value=-1, next=null, prev=null) {
+//         this.key = key;
+//         this.value = value;
+//         this.next = next;
+//         this.prev = prev;
+//     }
+// }
+
